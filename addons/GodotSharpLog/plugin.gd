6ffe53e53f51570834274d8db881d7e1ff2a02e3
luckyfeng <1101459726@qq.com>
2025-05-17 16:26:25 +0800

    删除翻译

--- addons/GodotSharpLog/plugin.gd
+++ addons/GodotSharpLog/plugin.gd
@@ -5,20 +5,7 @@ const LogDebugger = preload("res://addons/GodotSharpLog/Editor/LogDebugger.gd")
 
 var debugger: LogDebugger = null;
 
-func _enter_tree() -> void:		
-	var dir_path: String = "res://addons/GodotSharpLog/Translations/";
-	var dir: DirAccess = DirAccess.open(dir_path);
-	if dir != null:
-		dir.list_dir_begin();
-		var domain = TranslationServer.get_or_add_domain("godot.editor");
-		var path: String = dir.get_next();
-		while path != null && path != "":
-			if(path.ends_with(".translation")):
-				var res = load(dir_path +"/"+ path);
-				domain.add_translation(res);
-			path = dir.get_next();
-		dir.list_dir_end();
-	
+func _enter_tree() -> void:
 	debugger = LogDebugger.new();
 	add_debugger_plugin(debugger);	
 	
