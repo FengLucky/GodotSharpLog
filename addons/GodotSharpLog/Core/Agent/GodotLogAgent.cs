4884149be4231ff975e043c8a05b8b3ff3b9d5ac
luckyfeng <1101459726@qq.com>
2025-12-11 01:15:07 +0800

    Revert "插件改为 develop 分支更新"
    This reverts commit eaa2fe16291cece3c4db3d8d66fad13982ee475f.

---
+++ addons/GodotSharpLog/Core/Agent/GodotLogAgent.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Godot;
+
+namespace GDLog;
+
+public class GodotLogAgent() : ILogAgent
+{
+    public void AddCategory(string category)
+    {
+        
+    }
+
+    public void Info(object message, string category)
+    {
+        Log(LogLevel.Info,message,category);
+    }
+
+    public void Warn(object message, string category)
+    {
+        Log(LogLevel.Warn,message,category);
+    }
+
+    public void Error(object message, string category)
+    {
+        Log(LogLevel.Error,message,category);
+    }
+
+    public void Exception(Exception exception, string category)
+    {
+        Log(LogLevel.Exception,exception.Message,category);
+    }
+
+    private void Log(LogLevel level, object message, string category)
+    {
+        string content = !string.IsNullOrWhiteSpace(category) ? $"[{category}]" : string.Empty;
+        content += message;
+
+        switch (level)
+        {
+            case LogLevel.Info:
+                GD.Print(content);
+                break;
+            case LogLevel.Warn:
+                GD.PushWarning(content);
+                break;
+            case LogLevel.Error:
+            case LogLevel.Exception:
+                GD.PushError(content);
+                break;
+        }
+    }
+}
